082fcf8295bf86289701e9626689652a06366b30
Дмитрий Канатников <steve111@yandex.ru>
2017-08-15 11:32:54 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var gutil = require('gulp-util');
+var through = require('through2');
+var compile = require('dresscodejs/lib/DressCode');
+
+var pluginName = 'gulp-dresscode';
+
+module.exports = function (opts) {
+    opts = opts || {};
+
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
+
+        if (file.isStream()) {
+            cb(new gutil.PluginError(pluginName, 'Streaming not supported'));
+            return;
+        }
+
+        compile(file, opts.labels, opts.context)
+            .then(function (text) {
+                file.contents = new Buffer(text);
+                cb(null, file);
+            })
+            .catch(function (err) {
+                cb(new gutil.PluginError(pluginName, err, {
+                    fileName: file.path,
+                    showProperties: false
+                }));
+            });
+    });
+};
